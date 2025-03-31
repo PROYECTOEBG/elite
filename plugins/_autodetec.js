9fa45c1ea303d250ad12171135b0e832c44f058d
ProyectoEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2025-03-31 01:11:38 -0500

    Update _autodetec.js

--- plugins/_autodetec.js
+++ plugins/_autodetec.js
@@ -8,7 +8,7 @@ let handler = m => m
 handler.before = async function (m, { conn, participants, groupMetadata, isBotAdmin }) {
 
 if (!m.messageStubType || !m.isGroup) return
-let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => gataMenu)
+let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => sinfoto5)
 let img = await (await fetch(`${pp}`)).buffer()
 let usuario = `@${m.sender.split`@`[0]}`
 let chat = global.db.data.chats[m.chat]
