bd6ca16fe376c447d6c69ab16e2e6c235f941c6b
ProyectoEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2025-04-02 14:58:35 -0500

    Update grupo-hidetag.js

--- plugins/Grupos/grupo-hidetag.js
+++ plugins/Grupos/grupo-hidetag.js
@@ -26,7 +26,7 @@ const handler = async (m, { conn, text, participants, isOwner, isAdmin }) => {
     const quoted = m.quoted ? m.quoted : m;
     const mime = (quoted.msg || quoted).mimetype || '';
     const isMedia = /image|video|sticker|audio/.test(mime);
-    const watermark = '\n\n> BOT - BARBOZA';
+    const watermark = '\n©𝖤𝗅𝗂𝗍𝖾𝖡𝗈𝗍𝖦𝗅𝗈𝖻𝖺𝗅';
 
     if (isMedia) {
       const mediax = await quoted.download?.();
