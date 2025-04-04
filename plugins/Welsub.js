76ecb3343149c4f29828fdb08ab51cf5e14932df
ProyectoEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2025-04-04 03:14:50 -0500

    Update Welsub.js

--- plugins/Welsub.js
+++ plugins/Welsub.js
@@ -1,72 +1,71 @@
+import path from 'path'
+import fs from 'fs'
+
 let handler = m => m
 handler.before = async function (m, { conn, participants, groupMetadata, isBotAdmin }) {
-    if (!m.messageStubType || !m.isGroup) return
-
-    let pp;
-    try {
-        pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image');
-    } catch (e) {
-        // Si no tiene foto, usa la imagen local
-        pp = './src/sinfoto2.jpg';  // Ruta local de la imagen
-    }
+    let chat = global.db.data.chats[m.chat]
     
-    let img = await fs.promises.readFile(pp);  // Leemos la imagen desde el archivo
-    let usuario = `@${m.sender.split`@`[0]}`;
-    let chat = global.db.data.chats[m.chat];
+    // Verificamos si el mensaje es de bienvenida (messageStubType 27)
+    if (chat.welcome && m.messageStubType == 27 && this.user.jid != global.conn.user.jid) {
+        let userName = `${m.messageStubParameters[0].split`@`[0]}`
+        let subject = groupMetadata.subject
+        let descs = groupMetadata.desc || "😻 𝗦𝘂𝗽𝗲𝗿 𝗚𝗮𝘁𝗮𝗕𝗼𝘁-𝗠𝗗 😻"
+        
+        let userProfilePicUrl = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => null)
+        let imageUrl = userProfilePicUrl || './src/sinfoto2.jpg' // Si no hay imagen, usamos la predeterminada
 
-    // Bienvenida
-    if (chat.welcome && m.messageStubType == 27 && this.user.jid != global.conn.user.jid) { 
-        let subject = groupMetadata.subject;
-        let descs = groupMetadata.desc || "😻 𝗦𝘂𝗽𝗲𝗿 𝗚𝗮𝘁𝗮𝗕𝗼𝘁-𝗠𝗗 😻";
-        let userName = `${m.messageStubParameters[0].split`@`[0]}`;
-        let defaultWelcome = `*╭┈⊰* ${subject} *⊰┈ ✦*\n*┊✨ BIENVENIDO(A)!!*\n┊💖 @${userName}\n┊📄 *LEA LA DESCRIPCIÓN DEL GRUPO*\n*╰┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈ ✦*\n${descs}`;
+        let defaultWelcome = `*╭┈⊰* ${subject} *⊰┈ ✦*\n*┊✨ BIENVENIDO(A)!!*\n┊💖 @${userName}\n┊📄 *LEA LA DESCRIPCIÓN DEL GRUPO*\n*╰┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈ ✦*\n${descs}`
         let textWel = chat.sWelcome ? chat.sWelcome
-        .replace(/@user/g, `@${userName}`)
-        .replace(/@group/g, subject) 
-        .replace(/@desc/g, descs)
-        : defaultWelcome;
-            
-        await this.sendMessage(m.chat, { text: textWel, 
-        contextInfo:{
-            forwardingScore: 9999999,
-            isForwarded: true, 
-            mentionedJid:[m.sender, m.messageStubParameters[0]],
-            externalAdReply: {
-                showAdAttribution: true,
-                renderLargerThumbnail: true,
-                thumbnailUrl: pp,  // Usa la imagen local de la bienvenida
-                title: [wm, '😻 𝗦𝘂𝗽𝗲𝗿 ' + gt + ' 😻', '🌟 centergatabot.gmail.com'].getRandom(),
-                containsAutoReply: true,
-                mediaType: 1, 
-                sourceUrl: [canal1, canal2, canal3, canal4, yt, grupo1, grupo2, grupo_collab1, grupo_collab2, grupo_collab3, md].getRandom()
+            .replace(/@user/g, `@${userName}`)
+            .replace(/@group/g, subject)
+            .replace(/@desc/g, descs)
+            : defaultWelcome
+
+        await this.sendMessage(m.chat, {
+            text: textWel,
+            contextInfo: {
+                forwardingScore: 9999999,
+                isForwarded: true,
+                mentionedJid: [m.sender, m.messageStubParameters[0]],
+                externalAdReply: {
+                    showAdAttribution: true,
+                    renderLargerThumbnail: true,
+                    thumbnailUrl: imageUrl, // Usamos la imagen obtenida o la predeterminada
+                    title: [wm, '😻 𝗦𝘂𝗽𝗲𝗿 ' + gt + ' 😻', '🌟 centergatabot.gmail.com'].getRandom(),
+                    containsAutoReply: true,
+                    mediaType: 1,
+                    sourceUrl: [canal1, canal2, canal3, canal4, yt, grupo1, grupo2, grupo_collab1, grupo_collab2, grupo_collab3, md].getRandom()
+                }
             }
-        }}, { quoted: fkontak }) 
+        }, { quoted: fkontak })
     }
 
-    // Despedida
-    if (chat.welcome && (m.messageStubType == 28 || m.messageStubType == 32) && this.user.jid != global.conn.user.jid ) {
-        let subject = groupMetadata.subject;
-        let userName = `${m.messageStubParameters[0].split`@`[0]}`;
-        let defaultBye = `*╭┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈⊰*\n┊ @${userName}\n┊ *NO LE SABE AL GRUPO, CHAO!!* 😎\n*╰┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈⊰*`;
-        let textBye = chat.sBye ? chat.sBye
-        .replace(/@user/g, `@${userName}`)
-        .replace(/@group/g, subject)
-        : defaultBye;
+    // Despedida (messageStubType 28)
+    else if (chat.welcome && m.messageStubType == 28 && this.user.jid != global.conn.user.jid) {
+        let userName = `${m.messageStubParameters[0].split`@`[0]}`
+        let subject = groupMetadata.subject
+        let defaultBye = `*╭┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈⊰*\n┊ @${userName}\n┊ *NO LE SABE AL GRUPO, CHAO!!* 😎\n*╰┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈⊰*`
+        let textBye = chat.sBye ? chat.sBye.replace(/@user/g, `@${userName}`).replace(/@group/g, subject) : defaultBye
 
-        await this.sendMessage(m.chat, { text: textBye, 
-        contextInfo:{
-            forwardingScore: 9999999,
-            isForwarded: true, 
-            mentionedJid:[m.sender, m.messageStubParameters[0]],
-            externalAdReply: {
-                showAdAttribution: true,
-                renderLargerThumbnail: true,
-                thumbnailUrl: pp,  // Usa la imagen local de la despedida
-                title: [wm, '😻 𝗦𝘂𝗽𝗲𝗿 ' + gt + ' 😻', '🌟 centergatabot.gmail.com'].getRandom(),
-                containsAutoReply: true,
-                mediaType: 1, 
-                sourceUrl: [canal1, canal2, canal3, canal4, yt, grupo1, grupo2, grupo_collab1, grupo_collab2, grupo_collab3, md].getRandom()
+        await this.sendMessage(m.chat, {
+            text: textBye,
+            contextInfo: {
+                forwardingScore: 9999999,
+                isForwarded: true,
+                mentionedJid: [m.sender, m.messageStubParameters[0]],
+                externalAdReply: {
+                    showAdAttribution: true,
+                    renderLargerThumbnail: true,
+                    thumbnailUrl: imageUrl, // Usamos la imagen obtenida o la predeterminada
+                    title: [wm, '😻 𝗦𝘂𝗽𝗲𝗿 ' + gt + ' 😻', '🌟 centergatabot.gmail.com'].getRandom(),
+                    containsAutoReply: true,
+                    mediaType: 1,
+                    sourceUrl: [canal1, canal2, canal3, canal4, yt, grupo1, grupo2, grupo_collab1, grupo_collab2, grupo_collab3, md].getRandom()
+                }
             }
-        }}, { quoted: fkontak }) 
+        }, { quoted: fkontak })
     }
+
 }
+
+export default handler
