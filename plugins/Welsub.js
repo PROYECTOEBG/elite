4b6cfc3b25653cf586585873a29b3bd00f421417
ProyectoEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2025-04-04 02:36:54 -0500

    Update Welsub.js

--- plugins/Welsub.js
+++ plugins/Welsub.js
@@ -1,30 +1,36 @@
-if (chat.welcome && m.messageStubType == 27 && this.user.jid != global.conn.user.jid) {
-    let subject = groupMetadata.subject;
-    let userName = `${m.messageStubParameters[0].split`@`[0]}`;
-    let descs = groupMetadata.desc || "😻 𝗦𝘂𝗽𝗲𝗿 𝗚𝗮𝘁𝗮𝗕𝗼𝘁-𝗠𝗗 😻";
-    
-    let defaultWelcome = `*╭┈⊰* ${subject} *⊰┈ ✦*\n*┊✨ BIENVENIDO(A)!!*\n┊💖 @${userName}\n┊📄 *LEA LA DESCRIPCIÓN DEL GRUPO*\n*╰┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈ ✦*\n${descs}`;
-    let textWel = chat.sWelcome ? chat.sWelcome
-        .replace(/@user/g, `@${userName}`)
-        .replace(/@group/g, subject)
-        .replace(/@desc/g, descs)
-        : defaultWelcome;
+handler.before = async function (m, { conn, participants, groupMetadata, isBotAdmin }) {
+    // Agrega un log para ver el messageStubType
+    console.log("messageStubType:", m.messageStubType);
 
-    await this.sendMessage(m.chat, { 
-        text: textWel, 
-        contextInfo: {
-            forwardingScore: 9999999,
-            isForwarded: true, 
-            mentionedJid: [m.sender, m.messageStubParameters[0]],
-            externalAdReply: {
-                showAdAttribution: true,
-                renderLargerThumbnail: true,
-                thumbnailUrl: pp, 
-                title: [wm, '😻 𝗦𝘂𝗽𝗲𝗿 ' + gt + ' 😻', '🌟 centergatabot.gmail.com'].getRandom(),
-                containsAutoReply: true,
-                mediaType: 1, 
-                sourceUrl: [canal1, canal2, canal3, canal4, yt, grupo1, grupo2, grupo_collab1, grupo_collab2, grupo_collab3, md].getRandom()
+    // Aquí va el resto de tu código
+    if (chat.welcome && m.messageStubType == 27 && this.user.jid != global.conn.user.jid) {
+        let subject = groupMetadata.subject;
+        let userName = `${m.messageStubParameters[0].split`@`[0]}`;
+        let descs = groupMetadata.desc || "😻 𝗦𝘂𝗽𝗲𝗿 𝗚𝗮𝘁𝗮𝗕𝗼𝘁-𝗠𝗗 😻";
+
+        let defaultWelcome = `*╭┈⊰* ${subject} *⊰┈ ✦*\n*┊✨ BIENVENIDO(A)!!*\n┊💖 @${userName}\n┊📄 *LEA LA DESCRIPCIÓN DEL GRUPO*\n*╰┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈ ✦*\n${descs}`;
+        let textWel = chat.sWelcome ? chat.sWelcome
+            .replace(/@user/g, `@${userName}`)
+            .replace(/@group/g, subject)
+            .replace(/@desc/g, descs)
+            : defaultWelcome;
+
+        await this.sendMessage(m.chat, { 
+            text: textWel, 
+            contextInfo: {
+                forwardingScore: 9999999,
+                isForwarded: true, 
+                mentionedJid: [m.sender, m.messageStubParameters[0]],
+                externalAdReply: {
+                    showAdAttribution: true,
+                    renderLargerThumbnail: true,
+                    thumbnailUrl: pp, 
+                    title: [wm, '😻 𝗦𝘂𝗽𝗲𝗿 ' + gt + ' 😻', '🌟 centergatabot.gmail.com'].getRandom(),
+                    containsAutoReply: true,
+                    mediaType: 1, 
+                    sourceUrl: [canal1, canal2, canal3, canal4, yt, grupo1, grupo2, grupo_collab1, grupo_collab2, grupo_collab3, md].getRandom()
+                }
             }
-        }
-    }, { quoted: fkontak });
+        }, { quoted: fkontak });
+    }
 }
