60d4b5157a2f89bb94b47df7362838d6d802b216
ProyectoEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2025-04-04 02:51:21 -0500

    Update Welsub.js

--- plugins/Welsub.js
+++ plugins/Welsub.js
@@ -1,63 +1,77 @@
-let handler = m => m;
+let handler = m => m
 handler.before = async function (m, { conn, participants, groupMetadata, isBotAdmin }) {
+  // Verifica si el mensaje es de un grupo y si contiene el tipo adecuado
+  if (!m.messageStubType || !m.isGroup) return
 
-    if (!m.messageStubType || !m.isGroup) return;
-
-    let chat = global.db.data.chats[m.chat];
-    let userName = `${m.messageStubParameters[0].split`@`[0]}`;
-    let subject = groupMetadata.subject;
-    let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => gataMenu);
-    let img = await (await fetch(`${pp}`)).buffer();
-    let defaultWelcome = `*╭┈⊰* ${subject} *⊰┈ ✦*\n*┊✨ BIENVENIDO(A)!!*\n┊💖 @${userName}\n┊📄 *LEA LA DESCRIPCIÓN DEL GRUPO*\n*╰┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈ ✦*`;
-    let defaultBye = `*╭┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈⊰*\n┊ @${userName}\n┊ *NO LE SABE AL GRUPO, CHAO!!* 😎\n*╰┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈⊰*`;
-
-    if (chat.welcome && m.messageStubType == 27 && this.user.jid != global.conn.user.jid) { 
-        // Bienvenida
-        let textWel = chat.sWelcome ? chat.sWelcome
-            .replace(/@user/g, `@${userName}`)
-            .replace(/@group/g, subject)
-            : defaultWelcome;
-
-        await this.sendMessage(m.chat, { text: textWel, 
-            contextInfo: {
-                forwardingScore: 9999999,
-                isForwarded: true, 
-                mentionedJid:[m.sender, m.messageStubParameters[0]],
-                externalAdReply: {
-                    showAdAttribution: true,
-                    renderLargerThumbnail: true,
-                    thumbnailUrl: pp,
-                    title: [wm, '😻 𝗦𝘂𝗽𝗲𝗿 ' + gt + ' 😻', '🌟 centergatabot.gmail.com'].getRandom(),
-                    containsAutoReply: true,
-                    mediaType: 1, 
-                    sourceUrl: [canal1, canal2, canal3, canal4, yt, grupo1, grupo2, grupo_collab1, grupo_collab2, grupo_collab3, md].getRandom()
-                }
-            }
-        }, { quoted: fkontak }); 
-    }
-
-    if (chat.welcome && (m.messageStubType == 28 || m.messageStubType == 32) && this.user.jid != global.conn.user.jid) {
-        // Despedida
-        let textBye = chat.sBye ? chat.sBye
-            .replace(/@user/g, `@${userName}`)
-            .replace(/@group/g, subject)
-            : defaultBye;
-
-        await this.sendMessage(m.chat, { text: textBye, 
-            contextInfo: {
-                forwardingScore: 9999999,
-                isForwarded: true, 
-                mentionedJid:[m.sender, m.messageStubParameters[0]],
-                externalAdReply: {
-                    showAdAttribution: true,
-                    renderLargerThumbnail: true,
-                    thumbnailUrl: pp,
-                    title: [wm, '😻 𝗦𝘂𝗽𝗲𝗿 ' + gt + ' 😻', '🌟 centergatabot.gmail.com'].getRandom(),
-                    containsAutoReply: true,
-                    mediaType: 1, 
-                    sourceUrl: [canal1, canal2, canal3, canal4, yt, grupo1, grupo2, grupo_collab1, grupo_collab2, grupo_collab3, md].getRandom()
-                }
-            }
-        }, { quoted: fkontak });
-    }
+  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => gataMenu) // Obtiene la foto de perfil del miembro
+  let img = await (await fetch(`${pp}`)).buffer()
+  let usuario = `@${m.sender.split`@`[0]}` // Extrae el nombre de usuario sin el dominio
+  let chat = global.db.data.chats[m.chat]
+  let users = participants.map(u => conn.decodeJid(u.id))
+  
+  // Verifica si es un mensaje de bienvenida (messageStubType: 27)
+  if (chat.welcome && m.messageStubType == 27 && this.user.jid != global.conn.user.jid) {
+    let subject = groupMetadata.subject
+    let descs = groupMetadata.desc || "😻 𝗦𝘂𝗽𝗲𝗿 𝗚𝗮𝘁𝗮𝗕𝗼𝘁-𝗠𝗗 😻"
+    let userName = `${m.messageStubParameters[0].split`@`[0]}`
+    let defaultWelcome = `*╭┈⊰* ${subject} *⊰┈ ✦*\n*┊✨ BIENVENIDO(A)!!*\n┊💖 @${userName}\n┊📄 *LEA LA DESCRIPCIÓN DEL GRUPO*\n*╰┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈ ✦*\n${descs}`
+    
+    // Reemplaza los valores en el mensaje de bienvenida si se ha personalizado
+    let textWel = chat.sWelcome ? chat.sWelcome
+      .replace(/@user/g, `@${userName}`)
+      .replace(/@group/g, subject) 
+      .replace(/@desc/g, descs)
+      : defaultWelcome;
+      
+    // Envia el mensaje de bienvenida al grupo
+    await this.sendMessage(m.chat, { text: textWel, 
+      contextInfo: {
+        forwardingScore: 9999999,
+        isForwarded: true, 
+        mentionedJid: [m.sender, m.messageStubParameters[0]],
+        externalAdReply: {
+          showAdAttribution: true,
+          renderLargerThumbnail: true,
+          thumbnailUrl: pp, 
+          title: ['Super Bot', 'Bot de grupo', 'Grupo'].getRandom(),
+          containsAutoReply: true,
+          mediaType: 1, 
+          sourceUrl: 'https://youtube.com'
+        }
+      }
+    }, { quoted: fkontak })
+  }
+  
+  // Verifica si es un mensaje de despedida (messageStubType: 28)
+  else if (chat.welcome && m.messageStubType == 28 && this.user.jid != global.conn.user.jid) {
+    let subject = groupMetadata.subject
+    let userName = `${m.messageStubParameters[0].split`@`[0]}`
+    let defaultBye = `*╭┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈⊰*\n┊ @${userName}\n┊ *NO LE SABE AL GRUPO, CHAO!!* 😎\n*╰┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈⊰*`
+    
+    // Reemplaza los valores en el mensaje de despedida si se ha personalizado
+    let textBye = chat.sBye ? chat.sBye
+      .replace(/@user/g, `@${userName}`)
+      .replace(/@group/g, subject)
+      : defaultBye;
+    
+    // Envia el mensaje de despedida al grupo
+    await this.sendMessage(m.chat, { text: textBye, 
+      contextInfo: {
+        forwardingScore: 9999999,
+        isForwarded: true, 
+        mentionedJid: [m.sender, m.messageStubParameters[0]],
+        externalAdReply: {
+          showAdAttribution: true,
+          renderLargerThumbnail: true,
+          thumbnailUrl: pp, 
+          title: ['Super Bot', 'Bot de grupo', 'Grupo'].getRandom(),
+          containsAutoReply: true,
+          mediaType: 1, 
+          sourceUrl: 'https://youtube.com'
+        }
+      }
+    }, { quoted: fkontak })
+  }
 }
+
+export default handler;
