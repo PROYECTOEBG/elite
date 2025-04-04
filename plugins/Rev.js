27512f4d5d8f44ac4cff2c33889661aea4fba9c3
ProyectoEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2025-04-04 12:17:49 -0500

    Update Rev.js

--- plugins/Rev.js
+++ plugins/Rev.js
@@ -5,9 +5,32 @@ handler.before = async function (m, { conn, groupMetadata }) {
   if (m.messageStubType !== 20) return // 20 = Creación de grupo
   
   let subject = groupMetadata.subject || "el grupo"
-  let welcomeBot = `✨ ¡Hola a todos! Soy su nuevo bot en *${subject}*! 🤖\n\n👮 Recuerden seguir las reglas del grupo.\n💡 Si necesitan ayuda, escriban *#menu* para ver mis comandos.`
+  let welcomeBot = `🥇 ¡𝗛𝗢𝗟𝗔 𝗚𝗥𝗨𝗣𝗢!🥇
+¡Soy ${botName}, su nuevo asistente digital!
+━━━━━━━━━━━━━━━━━━━  
+⚡ *Mis funciones :*  
+▸  Descargar música/videos 
+▸  Búsquedas en Google/Wikipedia 
+▸  Juegos y diversión grupal 
+▸  Generar imágenes con IA 
+▸  Herramientas para Free Fire 
+━━━━━━━━━━━━━━━━━━━  
+📂 *Mis menus:*  
+▸  .menu → *Menú general*  
+▸  .menuimg → *Imágenes AI*  
+▸  .menuhot → *Contenido destacado*  
+▸  .menuaudios→ *Audios y efectos* 
+▸  .menujuegos → *Juegos grupales* 
+▸  .menufreefire → *Free Fire tools*
+━━━━━━━━━━━━━━━━━━━  
+©EliteBotGlobal 2023`
 
   await this.sendMessage(m.chat, { text: welcomeBot }, { quoted: m })
 }
 
 export default handler
+
+
+
+
+  
