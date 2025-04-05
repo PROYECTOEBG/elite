705ea59924320b533c163a8abff292feed1b5241
ProyectoEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2025-04-04 22:47:08 -0500

    Update comprar.js

--- plugins/Grupos/comprar.js
+++ plugins/Grupos/comprar.js
@@ -2,7 +2,7 @@ let handler = async (m, { isPrems, conn }) => {
 let time = global.db.data.users[m.sender].lastcofre + 0 // 36000000 10 Horas //86400000 24 Horas
 if (new Date - global.db.data.users[m.sender].lastcofre < 0) throw `[❗𝐈𝐍𝐅𝐎❗] 𝚈𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚂𝚃𝙴 𝚃𝚄 𝙲𝙾𝙵𝚁𝙴\𝚗𝚅𝚄𝙴𝙻𝚅𝙴 𝙴𝙽 *${msToTime(time - new Date())}* 𝙿𝙰𝚁𝙰 𝚅𝙾𝙻𝚅𝙴𝚁 𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚁`
 await m.react('💳')
-let img = 'https://qu.ax/SeOfc.jpg' 
+let img = './src/comprar.jpg'
 let texto = `𝙀𝙡𝙞𝙩𝙚𝘽𝙤𝙩𝙂𝙡𝙤𝙗𝙖𝙡 -
 
 Creado en el año 2023/07/15
