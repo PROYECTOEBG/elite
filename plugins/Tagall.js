39d96708561a0cc7367f3ef95db5128d67d4e213
ProyectoEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2025-04-05 14:24:42 -0500

    Update Tagall.js

--- plugins/Tagall.js
+++ plugins/Tagall.js
@@ -1,35 +1,11 @@
-let handler = async (m, { isOwner, isAdmin, conn, text, participants, args }) => {
-  if (!(isAdmin || isOwner)) {
-    global.dfail('admin', m, conn)
-    throw false
+let handler = async (m, { conn }) => {
+  try {
+    await conn.presenceSubscribe(m.sender)
+    m.reply('Tu bot **sí tiene acceso** a presenceSubscribe.')
+  } catch (e) {
+    console.error(e)
+    m.reply('Tu bot **NO tiene acceso** a presenceSubscribe.')
   }
-
-  let pesan = args.join` `
-  let oi = `📩 ${lenguajeGB['smsAddB5']()} ${pesan}`
-  let teks = `*╭━* ${lenguajeGB['smstagaa']()} \n\n${oi}\n`
-  teks += `👤 𝙈𝙄𝙀𝙈𝘽𝙍𝙊𝙎: *${participants.length}*\n\n`
-
-  for (let mem of participants) {
-    let id = mem.id
-    let jid = id
-
-    // Suscribirse a presencia (si el bot soporta esto)
-    await conn.presenceSubscribe(jid).catch(() => {}) // Ignorar errores
-    await new Promise(resolve => setTimeout(resolve, 150)) // pequeña espera
-
-    let estado = conn.presence && conn.presence[jid] && conn.presence[jid].lastKnownPresence || "offline"
-    let estadoTexto = estado === "available" ? "🟢 Online" : "🔴 Offline"
-
-    teks += `┃👤@${id.split('@')[0]} ${estadoTexto}\n`
-  }
-
-  teks += `\n*╰━* 𝙀𝙇𝙄𝙏𝙀 𝘽𝙊𝙏 𝙂𝙇𝙊𝘽𝘼𝙇\n▌│█║▌║▌║║▌║▌║▌║█`
-  conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 }
-
-handler.command = /^(kevin|invocar|invocacion|todos|invocación)$/i
-handler.admin = true
-handler.botAdmin = true
-handler.group = true
-
+handler.command = /^pruebaestado$/i
 export default handler
