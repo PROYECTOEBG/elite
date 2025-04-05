cd865db7ad6bb1cbf0e70a15bd3bb2b0a25048ed
ProyectoEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2025-04-04 23:13:17 -0500

    Update info-grupos-lista.js

--- plugins/Propietario/info-grupos-lista.js
+++ plugins/Propietario/info-grupos-lista.js
@@ -1,13 +1,5 @@
-import fs from 'fs'
-import PhoneNumber from 'awesome-phonenumber'
-
-const archivoGrupos = './datos_grupos.json'
-let datosGrupos = {}
-if (fs.existsSync(archivoGrupos)) {
-  datosGrupos = JSON.parse(fs.readFileSync(archivoGrupos))
-}
-
 let handler = async (m, { conn, isOwner }) => {
+  console.log("Comando ejecutado");
   const groups = Object.values(await conn.groupFetchAllParticipating())
   let txt = `*LISTA DE GRUPOS*\nTotal: ${groups.length}\n\n`
 
@@ -25,29 +17,3 @@ ${isOwner ? `*Participantes:* ${grupo.participants.length}` : ''}
 
   m.reply(txt.trim())
 }
-
-// Evento que maneja cuando el bot entra a un grupo
-conn.on('group-participants-update', async (update) => {
-  // Verifica si el bot ha sido añadido al grupo
-  if (update.action === 'add' && update.participants.includes(conn.user.jid)) {
-    const groupId = update.id
-    const fechaIngreso = new Date().toISOString()
-    const agregadoPor = update.addedBy
-
-    // Registrar la fecha de ingreso y quién agregó al bot
-    datosGrupos[groupId] = {
-      fechaIngreso: fechaIngreso,
-      agregadoPor: agregadoPor
-    }
-
-    // Guardar los datos actualizados en el archivo JSON
-    fs.writeFileSync(archivoGrupos, JSON.stringify(datosGrupos, null, 2))
-  }
-})
-
-handler.help = ['listagrupos']
-handler.tags = ['info']
-handler.command = /^(groups|grouplist|listagrupos)$/i
-handler.rowner = true
-handler.exp = 30
-export default handler
