cbd16622923471518e0adf39a03e456fe9b323bb
ProyectoEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2025-04-08 07:11:32 -0500

    Update propietario(a)-reporte.js

--- plugins/Propietario/propietario(a)-reporte.js
+++ plugins/Propietario/propietario(a)-reporte.js
@@ -1,4 +1,4 @@
-let handler = async (m, { conn, text, usedPrefix, command }) => {
+/*let handler = async (m, { conn, text, usedPrefix, command }) => {
 if (!text) throw `${mg}*𝙀𝙨𝙘𝙧𝙞𝙗𝙖 𝙚𝙡 𝙧𝙚𝙥𝙤𝙧𝙩𝙚*\n\n*𝙀𝙅𝙀𝙈𝙋𝙇𝙊:*\n*${usedPrefix + command} el comando ${usedPrefix}infobot no funciona.*\n\n*𝙒𝙧𝙞𝙩𝙚 𝙩𝙝𝙚 𝙧𝙚𝙥𝙤𝙧𝙩*\n\n*𝙀𝙓𝘼𝙈𝙋𝙇𝙀:*\n*${usedPrefix + command} the command ${usedPrefix}owner it does not work.*`
 if (text.length < 8) throw `${fg} ✨ *Mínimo 10 caracteres para hacer El Reporte.*\n\n✨ *Minimum 10 characters to make the Report.*`
 if (text.length > 1000) throw `${fg} 😼 *Máximo 1000 caracteres para hacer El Reporte.*\n\n😼 *Maximum 1000 characters to make the Report.*`
@@ -20,3 +20,4 @@ handler.tags = ['info']
 handler.exp = 25 
 handler.command = /^(report|request|reporte|bugs|bug|report-owner|reportes|reportar)$/i 
 export default handler
+*/
