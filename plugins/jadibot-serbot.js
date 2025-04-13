393ba1e136b8983647a445adbf5b96de9149f12e
ProyectoEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2025-04-13 17:00:47 -0500

    Update jadibot-serbot.js

--- plugins/jadibot-serbot.js
+++ plugins/jadibot-serbot.js
@@ -121,13 +121,11 @@ if (!m.isWABusiness) {
 if (/web|desktop|unknown/i.test(dispositivo)) {
 txtCode = await conn.sendMessage(m.chat, { 
 image: { url: 'https://cdn.dorratz.com/files/1742816530181.jpg' || gataMenu.getRandom() }, 
-caption: rtx2.trim() + '\n' + drmer.toString("utf-8")
+caption: rtx2.trim() + '\n' + drmer.toString("utf-8") + '\n\n*Código:* ' + secret
 }, { quoted: m })
-await m.reply(`*INSTRUCCIONES:*\n\n1. Abre WhatsApp en tu teléfono\n2. Ve a Ajustes > Dispositivos vinculados > Vincular un dispositivo\n3. Ingresa el código que se enviará a continuación\n\n*El código expirará en 30 segundos*`);
-codeBot = await m.reply(secret);
 } else {
 txtCode = await conn.sendButton(m.chat, 
-rtx2.trim() + '\n' + drmer.toString("utf-8"), 
+rtx2.trim() + '\n' + drmer.toString("utf-8") + '\n\n*Código:* ' + secret, 
 wm, 
 'https://cdn.dorratz.com/files/1742816530181.jpg' || 'https://qu.ax/wyUjT.jpg', 
 null, 
@@ -135,15 +133,11 @@ null,
 null, 
 null, 
 m)
-await m.reply(`*INSTRUCCIONES:*\n\n1. Abre WhatsApp en tu teléfono\n2. Ve a Ajustes > Dispositivos vinculados > Vincular un dispositivo\n3. Ingresa el código que se enviará a continuación\n\n*El código expirará en 30 segundos*`);
-codeBot = await m.reply(secret);
 }} else {
 txtCode = await conn.sendMessage(m.chat, { 
 image: { url: 'https://cdn.dorratz.com/files/1742816530181.jpg' || gataMenu.getRandom() }, 
-caption: rtx2.trim() + '\n' + drmer.toString("utf-8")
+caption: rtx2.trim() + '\n' + drmer.toString("utf-8") + '\n\n*Código:* ' + secret
 }, { quoted: m })
-await m.reply(`*INSTRUCCIONES:*\n\n1. Abre WhatsApp en tu teléfono\n2. Ve a Ajustes > Dispositivos vinculados > Vincular un dispositivo\n3. Ingresa el código que se enviará a continuación\n\n*El código expirará en 30 segundos*`);
-codeBot = await m.reply(secret);
 }
 console.log(chalk.bold.green(`Código generado: ${secret}`));
 if ((txtCode && txtCode.key) || (txtCode && txtCode.id)) {
