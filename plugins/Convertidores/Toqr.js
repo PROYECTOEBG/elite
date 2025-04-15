7e8b1823d75ab0a2264c1a8850b9ddc1ad524016
ProyectoEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2025-04-15 12:22:56 -0500

    Update Toqr.js

--- plugins/Convertidores/Toqr.js
+++ plugins/Convertidores/Toqr.js
@@ -1,15 +1,64 @@
-import uploadFile from '../../lib/uploadFile.js';
-import uploadImage from '../../lib/uploadImage.js';
-const handler = async (m) => {
-  const q = m.quoted ? m.quoted : m;
-  const mime = (q.msg || q).mimetype || '';
-  if (!mime) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙰 𝙰 𝚄𝙽𝙰 𝙸𝙼𝙰𝙶𝙴𝙽 𝙾 𝚅𝙸𝙳𝙴𝙾 𝙴𝙻 𝙲𝚄𝙰𝙻 𝚂𝙴𝚁𝙰 𝙲𝙾𝙽𝚅𝙴𝚁𝚃𝙸𝙳𝙾 𝙰 𝙴𝙽𝙻𝙰𝙲𝙴*';
-  const media = await q.download();
-  const isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime);
-  const link = await (isTele ? uploadImage : uploadFile)(media);
-  m.reply(`\`\`\`[ ⭐ ] Aquí tienes la URL de tu archivo:\`\`\`\n✅ ${link}`);
+import fetch from "node-fetch";
+import crypto from "crypto";
+import { FormData, Blob } from "formdata-node";
+import { fileTypeFromBuffer } from "file-type";
+
+let handler = async (m, { conn }) => {
+  let q = m.quoted ? m.quoted : m;
+  let mime = (q.msg || q).mimetype || '';
+  if (!mime) return conn.reply(m.chat, `Por favor, responde a un archivo válido (imagen, video, etc.).`, m);
+  
+  await m.react(💯);
+  
+  try {
+    let media = await q.download();
+    let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime);
+    let link = await catbox(media);
+    
+    let txt = `*乂 C A T B O X - U P L O A D E R 乂*\n\n`;
+    txt += `*» Enlace* : ${link}\n`;
+    txt += `*» Tamaño* : ${formatBytes(media.length)}\n`;
+    txt += `*» Expiración* : ${isTele ? 'No expira' : 'Desconocido'}\n\n`;
+    txt += `> *${dev}*`;
+    
+    await conn.sendFile(m.chat, media, 'thumbnail.jpg', txt, m, fkontak);
+    
+    await m.react(✅);
+  } catch {
+    await m.react(🍁);
+  }
 };
-handler.help = ['tourl <reply image>'];
-handler.tags = ['sticker'];
-handler.command = /^(upload|tourl)$/i;
+
+handler.help = ['tourl2'];
+handler.tags = ['transformador'];
+handler.command = ['catbox', 'tourl'];
 export default handler;
+
+function formatBytes(bytes) {
+  if (bytes === 0) {
+    return '0 B';
+  }
+  const sizes = ['B', 'KB', 'MB', 'GB', 'TB'];
+  const i = Math.floor(Math.log(bytes) / Math.log(1024));
+  return `${(bytes / 1024 ** i).toFixed(2)} ${sizes[i]}`;
+}
+
+async function catbox(content) {
+  const { ext, mime } = (await fileTypeFromBuffer(content)) || {};
+  const blob = new Blob([content.toArrayBuffer()], { type: mime });
+  const formData = new FormData();
+  const randomBytes = crypto.randomBytes(5).toString("hex");
+  formData.append("reqtype", "fileupload");
+  formData.append("fileToUpload", blob, randomBytes + "." + ext);
+
+  const response = await fetch("https://catbox.moe/user/api.php", {
+    method: "POST",
+    body: formData,
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36",
+    },
+  });
+
+  return await response.text();
+}
