dd7ae70738e85a76a372886fffad4e260104526e
ProyectoEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2025-04-19 12:14:56 -0500

    Update Allmenu.js

--- plugins/Allmenu.js
+++ plugins/Allmenu.js
@@ -1,9 +1,21 @@
 let handler = async (m, { conn }) => {
-    let texto = `╭━━━━━━━━━━━━━━━━━━━╮
-┃ EliteBot
-┃ MODALIDAD: CLK
-┃ ROPA: verde
-╰━━━━━━━━━━━━━━━━━━━╯
+    const sections = [
+        {
+            title: "BOLLLOBOT / MELDEXZZ.",
+            rows: [
+                {title: "Escuadra 1", rowId: ".escuadra1", description: "Seleccionar Escuadra 1"},
+                {title: "Escuadra 2", rowId: ".escuadra2", description: "Seleccionar Escuadra 2"},
+                {title: "Suplente", rowId: ".suplente", description: "Seleccionar Suplente"},
+                {title: "Limpiar lista", rowId: ".limpiarlista", description: "Limpiar todas las listas"}
+            ]
+        }
+    ]
+
+    const listMessage = {
+        text: `
+EliteBot
+MODALIDAD: CLK
+ROPA: verde
 
 Escuadra 1:
 👤 ➤
@@ -22,15 +34,14 @@ SUPLENTE:
 👤
 👤
 
-BOLLLOBOT / MELDEXZZ.
+BOLLLOBOT / MELDEXZZ.`,
+        footer: "Selecciona una opción:",
+        title: "EliteBot",
+        buttonText: "Seleccionar Opción",
+        sections
+    }
 
-Selecciona una opción:
-1. .escuadra1
-2. .escuadra2
-3. .suplente
-4. .limpiarlista`
-
-    await m.reply(texto)
+    await conn.sendMessage(m.chat, listMessage)
 }
 
 handler.command = ['listaff']
